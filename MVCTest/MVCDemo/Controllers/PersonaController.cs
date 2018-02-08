d965cd6427350c03ec448f1b9b298c86f3a5dc7b
Moises Alonso <maalonsog@correo.url.edu.gt>
2018-02-07 23:43:28 -0600

    Edit Function Added.

--- MVCTest/MVCDemo/Controllers/PersonaController.cs
+++ MVCTest/MVCDemo/Controllers/PersonaController.cs
@@ -3,6 +3,7 @@ using MVCDemo.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -33,6 +34,38 @@ namespace MVCDemo.Controllers
                 return RedirectToAction("Index");
             }
             return View(persona);
+        }
+
+        public ActionResult Edit(int? id) {
+            if (id == null) {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Persona persona = db.Personas.Find(x => x.PersonaID == id);
+
+            if (persona == null) {
+                return HttpNotFound();
+            }
+
+            return View(persona);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PersonaID,Nombre,Apellido,Edad")]Persona persona) {
+            if (ModelState.IsValid) {
+                Persona modifiedPersona = db.Personas.Find(x => x.PersonaID == persona.PersonaID);
+                if (modifiedPersona == null)
+                {
+                    return HttpNotFound();
+                }
+                modifiedPersona.Nombre = persona.Nombre;
+                modifiedPersona.Apellido = persona.Apellido;
+                modifiedPersona.Edad = persona.Edad;
+                return View(modifiedPersona);
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
